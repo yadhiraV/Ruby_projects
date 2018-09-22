0bdc160573b194bacbfe13641a5da8d4994237dd
linda <lin.duh121@gmail.com>
2018-09-21 21:27:02 -0700

    first commit

---
+++ name.rb
@@ -0,0 +1,5 @@
+firstname = ["Yadhira", "Rose", "Layla", "Lexa", "Linda" ]
+lastname = ["Rodriguez", "Vargas", "Ramirez", "Estrada", "Ceja"]
+item = firstname[rand(firstname.length)]
+item2 = lastname[rand(lastname.length)]
+puts item, item2
